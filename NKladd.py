f2ca28d6bb7dc91bb3a363b1985459b12b271db2
nlennartsson <natanael.c.lennartsson@gmail.com>
2025-05-20 16:17:35 +0200

    Found some thresholds for the birch clustering that yields desent results

--- NKladd.py
+++ NKladd.py
@@ -3,15 +3,26 @@ import numpy as np
 import matplotlib.pyplot as plt
 import dataSet as ds
 import Nkod as na
+import Fkod as fk
 
 cdFull, cdLabels, cdImagesMatrix, cdImagesList = ds.catdog()
 mFull, mLabels, mImagesMatrix, mImagesList = ds.mnist()
 
 #mnistLabels = len(set(mLabels))
 #catdogLabels = len(set(cdLabels))
+#print(f'\n Cat Dog')
+cdFtest = fk.FTestFeatureSelection(cdImagesList, 70)
+#print(cdFtest.shape)
+cdpca_X, cdpca = fk.select_by_pca(cdFtest, 2)
+#print(cdpca_X.shape)
+cdpred, cddist = na.birch(cdpca_X, cdLabels, 0.3)
 
-cdpred, cddist = na.birch(cdImagesList, cdLabels)
-mpred, mdist = na.birch(mImagesList, mLabels)
+#print(f'\n MNIST')
+mFtest = fk.FTestFeatureSelection(mImagesList, 200)
+#print(mFtest.shape)
+mpca_X, mpca = fk.select_by_pca(mFtest, 2)
+#print(mpca_X.shape)
+mpred, mdist = na.birch(mpca_X, mLabels, .2)
 
 #print(cdpred) 
 #print(mpred)
--- Nkod.py
+++ Nkod.py
@@ -3,18 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-def birch(dataset, labels):
+def birch(dataset, labels, thresh = 0.5):
     clusters = len(set(labels))
     
-    brc = Birch(n_clusters = clusters)
+    brc = Birch(threshold = thresh, n_clusters = clusters)
     brc.fit(dataset)
     prediction = brc.predict(dataset)
     distance = brc.fit_transform(dataset)
-    print(clusters)
-    print(f'predictions:{prediction.shape}, distance:{distance.shape}')
+    #print(clusters)
+    #print(f'predictions:{prediction.shape}, distance:{distance.shape}')
+    #print(dataset.shape)
     setLength = len(distance[1])
+    
     plt.figure()
-    plt.scatter(distance[0], distance[1], c=prediction[:setLength], marker='o')
+    plt.scatter(dataset[:setLength,0], distance[:setLength,1], c=prediction[:setLength], marker='o')
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Birch Clustering')
